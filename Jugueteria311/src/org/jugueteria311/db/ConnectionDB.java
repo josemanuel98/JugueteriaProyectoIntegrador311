a333aa46304df9a51ebdc025cd385d850c19ea88
Jose Manuel Martinez Lopez <josemanuelmartinezlopez@live.com.mx>
2017-11-19 20:15:31 -0600

    Agregada conexion a BD

--- Jugueteria311/src/org/jugueteria311/db/ConnectionDB.java
+++ Jugueteria311/src/org/jugueteria311/db/ConnectionDB.java
@@ -5,10 +5,34 @@
  */
 package org.jugueteria311.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Josem
  */
 public class ConnectionDB {
+    Connection conn;
     
+    public Connection open() throws Exception{
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection("Server=tcp:servidorintegrador.database.windows.net,1433;Initial Catalog=IntegradorJugueteria;Persist Security Info=False;User ID=EquipoIntegrador311;Password=UDLSB311Integrador;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            return conn;
+    }
+    
+    public void close(){
+        if(conn == null){
+            return;
+        }
+        try{
+            conn.close();
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        finally{
+            conn = null;
+        }
+    }
 }
